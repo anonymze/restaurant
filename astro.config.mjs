4753c1958a35bbd1f9d867ae1dc4d1371ea5361b
anonymze <anodevfr@gmail.com>
2025-01-03 01:36:35 +0100

    setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,25 @@
-import tailwindcss from "@tailwindcss/vite";
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig, envField } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwindcss()]
+	integrations: [react()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	experimental: {
+		responsiveImages: true,
+		svg: true,
+	},
+	env: {
+		schema: {
+			API_URL: envField.string({
+				context: "client",
+				access: "public",
+			}),
+		},
+	},
 });

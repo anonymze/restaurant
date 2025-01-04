2960d85529d5cc91b60930b5d7f61353aa5ab0f4
anonymze <anodevfr@gmail.com>
2025-01-04 16:36:31 +0100

    ok

--- src/components/animated-button.tsx
+++ src/components/animated-button.tsx
@@ -11,7 +11,7 @@ export default function AnimatedButton({ text, className }: AnimatedButtonProps)
   return (
     <motion.div
       whileHover={{ scale: 1.02 }}
-      transition={{ stiffness: 400, damping: 10, duration: 0.15 }}
+      transition={{ stiffness: 400, damping: 10, duration: 0.15}}
     >
       <Button className={className}>
         {text}

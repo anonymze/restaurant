edc7989e80cebf3718a8b0b9b7022ec7fefd5c06
anonymze <anodevfr@gmail.com>
2025-01-05 05:15:26 +0100

    ok

---
+++ src/components/hey.tsx
@@ -0,0 +1,38 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+
+export function TabsDemo() {
+	return (
+		<Tabs defaultValue="entries" className="mt-4 max-w-[800px] mx-auto">
+			<TabsList className="grid w-full grid-cols-3 gap-5">
+				<TabsTrigger
+					className="text-primary border border-primary rounded-none  px-4 py-2 font-bold text-lg [&[data-state=active]]:bg-primary [&[data-state=active]]:text-white"
+					value="entries"
+				>
+					Entrées
+				</TabsTrigger>
+				<TabsTrigger
+					className="text-primary border border-primary rounded-none px-4 py-2 font-bold text-lg [&[data-state=active]]:bg-primary [&[data-state=active]]:text-white"
+					value="plates"
+				>
+					Plats
+				</TabsTrigger>
+				<TabsTrigger
+					className="text-primary border border-primary rounded-none px-4 py-2 font-bold text-lg [&[data-state=active]]:bg-primary [&[data-state=active]]:text-white"
+					value="desserts"
+				>
+					Desserts
+				</TabsTrigger>
+			</TabsList>
+			<TabsContent value="entries">
+				<section aria-label="Présentation des entrées de la carte"></section>
+			</TabsContent>
+			<TabsContent value="plates">
+				<section aria-label="Présentation des plats de la carte"></section>
+			</TabsContent>
+			<TabsContent value="desserts">
+				<section aria-label="Présentation des desserts de la carte"></section>
+			</TabsContent>
+		</Tabs>
+	);
+}

3f7c29b4924c4fc3cc3a6a7411771183704ae0f0
anonymze <anodevfr@gmail.com>
2025-01-05 17:01:09 +0100

    ok

--- src/components/tabs-menu.tsx
+++ src/components/tabs-menu.tsx
@@ -1,88 +1,58 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 
-export function TabsMenu() {
+interface TabsMenuProps {
+	titles: string[];
+	content: {
+		image: string;
+		name: string;
+		description: string;
+		price: number;
+		allergens: boolean;
+	}[];
+}
+
+export function TabsMenu({ titles, content }: TabsMenuProps) {
 	return (
 		<Tabs defaultValue="entries" className="mt-6 max-w-[800px] mx-auto px-4">
 			<p className="my-5 text-xs ">
 				<span className="text-red-700">*</span> Signifie une présence d'allergènes
 			</p>
 			<TabsList className="grid w-full grid-cols-3 gap-4 h-11 p-0 mb-10 md:gap-5 ">
-				<TabsTrigger
-					className="h-full text-primary border border-primary rounded-none font-bold data-[state=active]:bg-primary [&[data-state=active]>*]:text-white"
-					value="entries"
-				>
-						<h2 className="text-lg">Entrées</h2>
+				{titles.map((title) => (
+					<TabsTrigger
+						key={title}
+						className="h-full text-primary border border-primary rounded-none font-bold data-[state=active]:bg-primary [&[data-state=active]>h2]:text-white"
+						value="entries"
+					>
+						<h2 className="text-lg">{title}</h2>
 					</TabsTrigger>
-				<TabsTrigger
-					className="h-full text-primary border border-primary rounded-none font-bold data-[state=active]:bg-primary [&[data-state=active]>*]:text-white"
-					value="plates"
-				>
-					<h2 className="text-lg">Plats</h2>
-				</TabsTrigger>
-				<TabsTrigger
-					className="h-full text-primary border border-primary rounded-none font-bold data-[state=active]:bg-primary [&[data-state=active]>*]:text-white"
-					value="desserts"
-				>
-					<h2 className="text-lg">Desserts</h2>
-				</TabsTrigger>
+				))}
 			</TabsList>
-			<TabsContent value="entries">
-				<section aria-label="Présentation des entrées de la carte">
-					{entries.map((entry) => (
-						<ArticleComponent key={entry.name} {...entry} />
-					))}
-				</section>
-			</TabsContent>
-			<TabsContent value="plates">
-				<section aria-label="Présentation des plats de la carte">
-					{entries.map((entry) => (
-						<ArticleComponent key={entry.name} {...entry} />
-					))}
-				</section>
-			</TabsContent>
-			<TabsContent value="desserts">
-				<section aria-label="Présentation des desserts de la carte">
-					{entries.map((entry) => (
-						<ArticleComponent key={entry.name} {...entry} />
-					))}
-				</section>
-			</TabsContent>
+			<section aria-label="Présentation des entrées de la carte">
+				{content.map((entry) => (
+					<article className="flex flex-row items-center gap-8 h-24 mb-6 ">
+						<img
+							className="object-cover"
+							src="https://placehold.jp/150x150.png"
+							alt={entry.name}
+							width={96}
+							height={96}
+						/>
+
+						<div className="flex flex-col flex-1 h-full border-b border-gray-400/20 gap-2">
+							<h3 className="text-lg font-bold">{entry.name}</h3>
+							<p className="text-sm text-gray-500">
+								{entry.description} {entry.allergens && <span className="text-red-700">*</span>}
+							</p>
+						</div>
+						<p className="text-primary font-bold ml-auto">{entry.price} €</p>
+					</article>
+				))}
+			</section>
 		</Tabs>
 	);
 }
-
-const ArticleComponent = ({
-	name,
-	description,
-	price,
-	allergens,
-}: {
-	name: string;
-	description: string;
-	price: number;
-	allergens: boolean;
-}) => {
-	return (
-		<article className="flex flex-row items-center gap-4 h-24 mb-6 ">
-			<img
-				className="object-cover"
-				src="https://placehold.jp/150x150.png"
-				alt={name}
-				width={96}
-				height={96}
-			/>
-
-			<div className="flex flex-col flex-1 h-full border-b border-gray-400/20 gap-2">
-				<h3 className="text-lg font-bold">{name}</h3>
-				<p className="text-sm text-gray-500">
-					{description} {allergens && <span className="text-red-700">*</span>}
-				</p>
-			</div>
-			<p className="text-primary font-bold ml-auto">{price} €</p>
-		</article>
-	);
-};
 
 const entries = [
 	{

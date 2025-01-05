e2add77179c8c3125bc61399fb868244ff5f48bf
anonymze <anodevfr@gmail.com>
2025-01-05 02:43:57 +0100

    ok

--- src/components/animated-button.tsx
+++ src/components/animated-button.tsx
@@ -1,22 +1,24 @@
-import { Button } from "@/components/ui/button";
+import { cn } from "@/utils/libs/tailwaind";
 import { motion } from "framer-motion";
 
 
 interface AnimatedButtonProps {
-  text: string;
-  className?: string;
+	children: React.ReactNode;
+	title: string;
+	href: string;
+	className?: string;
 }
 
-export default function AnimatedButton({ text, className }: AnimatedButtonProps) {
-  return (
-    <motion.div
-		className="w-fit h-fit mx-auto"
-      whileHover={{ scale: 1.05 }}
-      transition={{ stiffness: 400, damping: 10, duration: 0.10}}
-    >
-      <Button variant="outline" className={className}>
-        {text}
-      </Button>
-    </motion.div>
-  );
-} 
+export default function AnimatedButton({ children, title, href, className }: AnimatedButtonProps) {
+	return (
+		<motion.a
+			href={href}
+			title={title}
+			whileHover={{ scale: 1.05 }}
+			transition={{ stiffness: 400, damping: 10, duration: 0.1 }}
+			className={cn("h-fit px-6 py-2 border-1 border-white text-white uppercase md:py-3 ", className)}
+		>
+			{children}
+		</motion.a>
+	);
+}

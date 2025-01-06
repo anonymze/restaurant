f4b20204f57bc0908d6deeb41eb14b6606bd4262
anonymze <anodevfr@gmail.com>
2025-01-06 19:30:17 +0100

    ok

--- src/components/select-people.tsx
+++ src/components/select-people.tsx
@@ -4,17 +4,17 @@ import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrig
 export function SelectPeople() {
 	return (
 		<Select>
-			<SelectTrigger className="w-full pl-12">
+			<SelectTrigger className="w-full pl-12 rounded-none">
 				<SelectValue placeholder="Sélectionnez combien vous êtes" />
 			</SelectTrigger>
-			<SelectContent className="bg-white">
-				<SelectItem className="hover:bg-primary" value="1">1 personne</SelectItem>
-				<SelectItem className="hover:bg-primary" value="2">2 personnes</SelectItem>
-				<SelectItem className="hover:bg-primary" value="3">3 personnes</SelectItem>
-				<SelectItem className="hover:bg-primary" value="4">4 personnes</SelectItem>
-				<SelectItem className="hover:bg-primary" value="5">5 personnes</SelectItem>
-				<SelectItem className="hover:bg-primary" value="6">6 personnes</SelectItem>
-				<SelectItem className="hover:bg-primary" value="7+">7 personnes ou plus</SelectItem>
+			<SelectContent className="bg-white rounded-none">
+				<SelectItem className="hover:bg-primary hover:text-white" value="1">1 personne</SelectItem>
+				<SelectItem className="hover:bg-primary hover:text-white" value="2">2 personnes</SelectItem>
+				<SelectItem className="hover:bg-primary hover:text-white" value="3">3 personnes</SelectItem>
+				<SelectItem className="hover:bg-primary hover:text-white" value="4">4 personnes</SelectItem>
+				<SelectItem className="hover:bg-primary hover:text-white" value="5">5 personnes</SelectItem>
+				<SelectItem className="hover:bg-primary hover:text-white" value="6">6 personnes</SelectItem>
+				<SelectItem className="hover:bg-primary hover:text-white" value="7+">7 personnes ou plus</SelectItem>
 			</SelectContent>
 		</Select>
 	);

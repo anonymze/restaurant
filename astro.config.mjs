6f6b80c1e48849460d5d8878c21fc93adb7b98a5
anonymze <anodevfr@gmail.com>
2025-01-07 00:13:57 +0100

    ini

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,28 @@
 // @ts-check
 import { defineConfig, envField } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
+import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
 
 // https://astro.build/config
 export default defineConfig({
 	site: "http://localhost:4321",
-	integrations: [react(), sitemap()],
+	integrations: [
+		react(),
+		sitemap(),
+		sanity({
+			projectId: "ld33nh8g",
+			dataset: "production",
+			useCdn: false, // for static builds
+		}),
+	],
 	vite: {
+        // @ts-ignore
 		plugins: [tailwindcss()],
 	},
-	
+
 	experimental: {
 		responsiveImages: true,
 		svg: true,

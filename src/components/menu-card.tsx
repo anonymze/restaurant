8f590f2d7e3f187a6cca825d8789281d78f4654c
anonymze <anodevfr@gmail.com>
2025-01-07 11:23:16 +0100

    ini

--- src/components/menu-card.tsx
+++ src/components/menu-card.tsx
@@ -1,88 +1,65 @@
 import { CalendarDays, Leaf, Soup, CakeSliceIcon, UtensilsCrossedIcon } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import type { SanityDocument } from "@sanity/client";
 import { Badge } from "@/components/ui/badge";
 import type { Menu } from "@/types/menu";
 
 
-export default function MenuPage({menu}: {menu: SanityDocument<Menu>[]}) {
+interface Props {
+	currentDay: string;
+	entries: Menu["menuItems"];
+	plates: Menu["menuItems"];
+	desserts: Menu["menuItems"];
+}
 
+export default function MenuPage({ currentDay, entries, plates, desserts }: Props) {
 	return (
-		<>
-			<Card className="mb-8 bg-tertiary/20 border-tertiary shadow-md rounded-none">
+			<Card className="mb-8 bg-tertiary/20 border-tertiary shadow-md">
 				<CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2 border-b border-tertiary">
-					<CardTitle className="text-2xl font-serif text-tertiary">{currentMenu.day}</CardTitle>
+					<CardTitle className="text-2xl text-tertiary">
+						Menu du <span className="capitalize">{currentDay}</span>
+					</CardTitle>
 					<CalendarDays className="h-6 w-6 text-tertiary" />
 				</CardHeader>
 				<CardContent className="pt-6">
-					<div className="space-y-12">
-						{menu.map(([category, items]) => (
-							<div key={category}>
-								<h3 className="text-xl font-serif text-tertiary mb-4 capitalize flex items-center">
-									{category === "entrées" ? (
-										<Soup className="h-5 w-5 mr-2" />
-									) : category === "plats" ? (
-										<UtensilsCrossedIcon className="h-5 w-5 mr-2" />
-									) : (
-										<CakeSliceIcon className="h-5 w-5 mr-2" />
-									)}
-									{category}
-								</h3>
-								<ul className="space-y-4">
-									{menu.menuItems.map((item) => (
-										<li key={item.} className="border-b border-tertiary/20 pb-4 last:border-b-0 last:pb-0">
-											<div className="flex justify-between items-start">
-												<div>
-													<h4 className="font-medium text-stone-800">{item.name}</h4>
-													<p className="text-sm text-stone-600">{item.description} {item.allergens && <span className="text-red-700">*</span>}</p>
-													{item.dietary && (
-														<div className="mt-1 space-x-1">
-															{item.dietary.map((diet) => (
-																<Badge
-																	key={diet}
-																	variant="outline"
-																	className="text-xs bg-tertiary/10 text-tertiary border-tertiary"
-																>
-																	{diet}
-																</Badge>
-															))}
-														</div>
-													)}
-												</div>
-												<span className="font-medium text-tertiary">{item.price} €</span>
-											</div>
-										</li>
-									))}
-								</ul>
-							</div>
-						))}
+					<div className="space-y-10">
+						{contentMenu(entries, "Entrées", <Soup className="h-5 w-5 mr-2" />)}
+						{contentMenu(plates, "Plats", <UtensilsCrossedIcon className="h-5 w-5 mr-2" />)}
+						{contentMenu(desserts, "Desserts", <CakeSliceIcon className="h-5 w-5 mr-2" />)}
 					</div>
 				</CardContent>
 			</Card>
-
-			<div className="p-6 shadow-sm border border-tertiary">
-				<h3 className="text-xl font-serif text-tertiary mb-4 flex items-center">
-					<Leaf className="h-5 w-5 mr-2" />
-					Informations sur le menu
-				</h3>
-				<ul className="space-y-2 text-stone-700 list-disc list-inside">
-					<li>
-						Le menu change quotidiennement en fonction des arrivages et de l'inspiration du chef.
-					</li>
-					<li>
-						Tous nos plats sont préparés sur place à partir de produits frais et de saison.
-					</li>
-					<li>
-						Pour toute allergie ou régime alimentaire spécifique, n'hésitez pas à nous consulter.
-					</li>
-					<li>
-						Nous essayons au maximum de proposer des plats végétariens en alternative.
-					</li>
-					<li>
-						Le vendredi le menu sera composé de poisson.
-					</li>
-				</ul>
-			</div>
-		</>
 	);
 }
+
+const contentMenu = (content: Menu["menuItems"], type: string, Icon: React.ReactNode) => {
+	return (
+		<div>
+			<h3 className="text-xl font-serif text-tertiary mb-4 capitalize flex items-center">
+				{Icon}
+				{type}
+			</h3>
+			<ul className="space-y-4">
+				{content.map((item) => (
+					<li key={item.title} className="border-b border-tertiary/20 pb-4 last:border-b-0 last:pb-0">
+						<div className="flex justify-between items-start">
+							<div>
+								<h4 className="font-medium text-stone-800">{item.title}</h4>
+								<p className="text-sm text-stone-600">
+									{item.description} {item.allergens && <span className="text-red-700">*</span>}
+								</p>
+								{item.vegetarian && (
+									<div className="mt-1 space-x-1">
+										<Badge variant="outline" className="text-xs bg-tertiary/10 text-tertiary border-tertiary">
+											végétarien
+										</Badge>
+									</div>
+								)}
+							</div>
+							<span className="font-medium text-tertiary">{item.price} €</span>
+						</div>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
--- src/types/menu.ts
+++ src/types/menu.ts
@@ -1,8 +1,11 @@
 export interface Menu {
 	date: string;
   menuItems: {
+    category: "entry" | "plate" | "dessert";
     title: string;
     description: string;
     price: number;
+    vegetarian?: boolean;
+    allergens?: boolean
   }[];
 } 

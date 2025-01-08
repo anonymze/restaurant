d6938d1d4e4fbd23268cb667c2afd05d31f2499e
anonymze <anodevfr@gmail.com>
2025-01-08 17:51:10 +0100

    ini

---
+++ api/send-email.ts
@@ -0,0 +1,62 @@
+import sgMail from "@sendgrid/mail";
+
+
+export const POST = async (request) => {
+	sendEmailTo({
+		apiKey: process.env.SENDGRID_API_KEY!,
+		sendEmailTo: 'metier.game@gmail.com',
+		templateId: process.env.SENDGRID_EMAIL_RESERVATION_NOTIFICATION!,
+		data: {
+			name: 'John Doe',
+			email: 'john.doe@example.com',
+		},
+	})
+
+
+	return new Response('OK', { status: 200 })
+}
+
+
+
+type SendEmailTo =
+	| {
+			statusCode: number;
+			headers: any;
+			error?: never;
+	  }
+	| {
+			error: any;
+			statusCode?: never;
+			headers?: never;
+	  };
+
+export const sendEmailTo = async ({
+	apiKey,
+	sendEmailTo,
+	templateId,
+	data,
+}: {
+	apiKey: string;
+	sendEmailTo: string;
+	templateId: string;
+	data?: Record<string, string>;
+}): Promise<SendEmailTo> => {
+	const msg = {
+		to: "metier.game@gmail.com",
+		// to: sendEmailTo,
+		// from: EMAIL_CONTACT,
+		// TODO
+		from: "metier.yann@gmail.com",
+		templateId: templateId,
+		dynamic_template_data: data,
+	};
+
+	sgMail.setApiKey(apiKey);
+
+	return sgMail.send(msg).then((response) => {
+		return {
+			statusCode: response[0].statusCode,
+			headers: response[0].headers,
+		};
+	});
+};

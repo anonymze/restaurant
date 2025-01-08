0231bcdf30ce0ecd0c91521a7cbfc25582b467a1
anonymze <anodevfr@gmail.com>
2025-01-08 13:16:21 +0100

    ini

--- api/send-email.ts
+++ api/send-email.ts
@@ -1,8 +1,8 @@
 
 
-export const config = {
-	runtime: "edge",
-};
+// export const config = {
+// 	runtime: "edge",
+// };
 
 export default async function handler(request: Request) {
 	console.log('coucou!!!!!');

eb9946ec5440008ac925ebff87a4d9626a9eb6da
anonymze <anodevfr@gmail.com>
2025-01-20 00:45:43 +0100

    ini

--- src/types/wine.ts
+++ src/types/wine.ts
@@ -1,14 +1,3 @@
 export interface Wine {
-	_updatedAt: string;
-	file: {
-		_type: string;
-		asset: {
-			_ref: string;
-			_type: string;
-		};
-	};
-	_createdAt: string;
-	_rev: string;
-	_type: string;
-	_id: string;
+	file: string;
 }

d2fe7077700de49bb37ea801224bed11b8d67dd9
anonymze <anodevfr@gmail.com>
2025-01-20 15:00:11 +0100

    ini

--- src/components/animated-link.tsx
+++ src/components/animated-link.tsx
@@ -14,8 +14,8 @@ export default function AnimatedLink({ children, title, href, className }: Anima
 		<motion.a
 			href={href}
 			title={title}
-			whileHover={{ scale: 1.05 }}
-			transition={{ stiffness: 400, damping: 10, duration: 0.1 }}
+			whileHover={{ scale: 1.02 }}
+			transition={{ stiffness: 400, damping: 10, duration: 0.2 }}
 			className={cn("h-fit px-6 py-2 border-1 border-white text-white uppercase md:py-3 ", className)}
 		>
 			{children}

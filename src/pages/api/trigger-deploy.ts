4cfea9a7650a3fa75c94d6056d023ae8eaf77761
anonymze <anodevfr@gmail.com>
2025-01-21 17:05:47 +0100

    deploy

--- src/pages/api/trigger-deploy.ts
+++ src/pages/api/trigger-deploy.ts
@@ -26,3 +26,32 @@ export async function GET(req: any) {
 		message: "Deploy triggered successfully",
 	});
 }
+
+
+export async function POST(req: any) {
+
+  // try {
+  //   const response = await fetch(
+  //     `https://api.vercel.com/v1/projects/${process.env.VERCEL_PROJECT_ID}/deployments`, 
+  //     {
+  //       method: 'POST',
+  //       headers: {
+  //         'Authorization': `Bearer ${process.env.VERCEL_DEPLOY_HOOK_TOKEN}`,
+  //         'Content-Type': 'application/json',
+  //       },
+  //     }
+  //   );
+
+  //   if (!response.ok) {
+  //     throw new Error('Deploy failed');
+  //   }
+
+  //   return new Response('Deploy triggered successfully', { status: 200 });
+  // } catch (error) {
+  //   return new Response('Deploy failed', { status: 500 });
+  // }
+
+	return Response.json({
+		message: "Deploy triggered successfully",
+	});
+}

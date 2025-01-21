0b4a21bd7645cce156bb20c398f3ed3406492e80
anonymze <anodevfr@gmail.com>
2025-01-21 18:19:41 +0100

    deploy

--- src/pages/api/trigger-deploy.ts
+++ src/pages/api/trigger-deploy.ts
@@ -1,6 +1,5 @@
 // the function can't be asynchrone on "static" mode i guess
 export function GET() {
 	fetch("https://api.vercel.com/v1/integrations/deploy/prj_5IzJAWAbUpd7AJpRAWqRLDPkP91T/gUWxx2NKgE");
-
 	return new Response("OK", { status: 200 });
 }

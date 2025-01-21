01f8318fed2c7a51021860e18abe185663c7828a
anonymze <anodevfr@gmail.com>
2025-01-21 12:50:12 +0100

    ini

---
+++ src/pages/trigger-deploy.js
@@ -0,0 +1,23 @@
+export default async function handler() {
+	console.log(process.env.VERCEL_PROJECT_ID);
+  try {
+    const response = await fetch(
+      `https://api.vercel.com/v1/projects/${process.env.VERCEL_PROJECT_ID}/deployments`, 
+      {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${process.env.VERCEL_DEPLOY_HOOK_TOKEN}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Deploy failed');
+    }
+
+    return new Response('Deploy triggered successfully', { status: 200 });
+  } catch (error) {
+    return new Response('Deploy failed', { status: 500 });
+  }
+}

9d51eda69b20cd70f8f5e6fb5396d5dc9ad1da8f
anonymze <anodevfr@gmail.com>
2025-01-21 17:14:19 +0100

    deploy

--- src/pages/api/trigger-deploy.ts
+++ src/pages/api/trigger-deploy.ts
@@ -1,26 +1,29 @@
+import type { APIContext } from "astro";
 
-export async function GE(req: any) {
 
-  // try {
-  //   const response = await fetch(
-  //     `https://api.vercel.com/v1/projects/${process.env.VERCEL_PROJECT_ID}/deployments`, 
-  //     {
-  //       method: 'POST',
-  //       headers: {
-  //         'Authorization': `Bearer ${process.env.VERCEL_DEPLOY_HOOK_TOKEN}`,
-  //         'Content-Type': 'application/json',
-  //       },
-  //     }
-  //   );
+export async function GET({ request }: APIContext) {
+	console.log(request);
 
-  //   if (!response.ok) {
-  //     throw new Error('Deploy failed');
-  //   }
+	// try {
+	//   const response = await fetch(
+	//     `https://api.vercel.com/v1/projects/${process.env.VERCEL_PROJECT_ID}/deployments`, 
+	//     {
+	//       method: 'POST',
+	//       headers: {
+	//         'Authorization': `Bearer ${process.env.VERCEL_DEPLOY_HOOK_TOKEN}`,
+	//         'Content-Type': 'application/json',
+	//       },
+	//     }
+	//   );
 
-  //   return new Response('Deploy triggered successfully', { status: 200 });
-  // } catch (error) {
-  //   return new Response('Deploy failed', { status: 500 });
-  // }
+	//   if (!response.ok) {
+	//     throw new Error('Deploy failed');
+	//   }
+
+	//   return new Response('Deploy triggered successfully', { status: 200 });
+	// } catch (error) {
+	//   return new Response('Deploy failed', { status: 500 });
+	// }
 
 	return Response.json({
 		message: "Deploy triggered successfully",

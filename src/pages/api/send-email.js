7f03f4a7ec4fad60df40fbc15cd36b573b0ddffc
anonymze <anodevfr@gmail.com>
2025-01-21 16:35:14 +0100

    deploy

--- src/pages/api/send-email.js
+++ src/pages/api/send-email.js
@@ -29,23 +29,23 @@ export async function POST(req) {
 	return new Response("OK", { status: 200 });
 };
 
-export const sendEmailTo = async ({ apiKey, templateId, data }) => {
-	const msg = {
-		// TODO "email restaurateur"
-		to: "metier.yann@gmail.com",
-		// TODO "email domain name"
-		from: "metier.game@gmail.com",
-		templateId,
-		dynamic_template_data: data,
-	};
+// export const sendEmailTo = async ({ apiKey, templateId, data }) => {
+// 	const msg = {
+// 		// TODO "email restaurateur"
+// 		to: "metier.yann@gmail.com",
+// 		// TODO "email domain name"
+// 		from: "metier.game@gmail.com",
+// 		templateId,
+// 		dynamic_template_data: data,
+// 	};
 
-	sgMail.setApiKey(apiKey);
+// 	sgMail.setApiKey(apiKey);
 
-	return sgMail.send(msg).then((response) => {
-		console.log(response);
-		return {
-			statusCode: response[0].statusCode,
-			headers: response[0].headers,
-		};
-	});
-};
+// 	return sgMail.send(msg).then((response) => {
+// 		console.log(response);
+// 		return {
+// 			statusCode: response[0].statusCode,
+// 			headers: response[0].headers,
+// 		};
+// 	});
+// };

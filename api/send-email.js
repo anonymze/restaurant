536d854df2981c1841bfe1e74ff5a793bb3ec904
anonymze <anodevfr@gmail.com>
2025-01-22 18:32:13 +0100

    ini

--- api/send-email.js
+++ api/send-email.js
@@ -1,7 +1,46 @@
-export async function POST(request) {
-	const { email } = await request.json();
+import sgMail from "@sendgrid/mail";
 
 
+export const POST = async (req) => {
+	const { email } = await req.json();
 
-	return new Response(email, { status: 200 });
-}
+	if (!email) {
+		return new Response("KO", { status: 400 });
+	}
+
+	try {
+		await sendEmailTo({
+			apiKey: process.env.SENDGRID_API_KEY,
+			sendEmailTo: "metier.yann@gmail.com",
+			templateId: process.env.SENDGRID_EMAIL_RESERVATION_NOTIFICATION,
+			data: {
+				email,
+			},
+		});
+	} catch (error) {
+		return new Response("KO", { status: 500 });
+	}
+
+	return new Response("OK", { status: 200 });
+};
+
+export const sendEmailTo = async ({ apiKey, templateId, data }) => {
+	const msg = {
+		// TODO "email restaurateur"
+		to: "metier.yann@gmail.com",
+		// TODO "email domain name"
+		from: "metier.game@gmail.com",
+		templateId,
+		dynamic_template_data: data,
+	};
+
+	sgMail.setApiKey(apiKey);
+
+	return sgMail.send(msg).then((response) => {
+		console.log(response);
+		return {
+			statusCode: response[0].statusCode,
+			headers: response[0].headers,
+		};
+	});
+};

bc49d3066451ce0d0db96f4294419f23c7097e13
anonymze <anodevfr@gmail.com>
2025-01-29 11:52:22 +0100

    ini

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,43 +7,42 @@ import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
 
+import simpleStackQuery from "simple-stack-query";
+
+
 // https://astro.build/config
 export default defineConfig({
-	i18n: {
+    i18n: {
     locales: ["fr", "en"],
     defaultLocale: "fr",
   },
-	site: "https://www.mauvaise-herbe-restaurant.fr",
-	adapter: vercel({
-		webAnalytics: {
-			enabled: true,
-		},
-	}),
-	output: "static",
-	integrations: [
-		react(),
-		sitemap(),
-		sanity({
-			projectId: "ld33nh8g",
-			dataset: "production",
-			useCdn: false, // for static builds, better for development when content changes frequently
-		}),
-	],
-	vite: {
-		// @ts-ignore
-		plugins: [tailwindcss()],
-	},
+    site: "https://www.mauvaise-herbe-restaurant.fr",
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true,
+        },
+    }),
+    output: "static",
+    integrations: [react(), sitemap(), sanity({
+        projectId: "ld33nh8g",
+        dataset: "production",
+        useCdn: false, // for static builds, better for development when content changes frequently
+		}), simpleStackQuery()],
+    vite: {
+        // @ts-ignore
+        plugins: [tailwindcss()],
+    },
 
-	experimental: {
-		responsiveImages: true,
-		svg: true,
-	},
-	env: {
-		schema: {
-			EMAIL_CONTACT: envField.string({
-				context: "client",
-				access: "public",
-			}),
-		},
-	},
+    experimental: {
+        responsiveImages: true,
+        svg: true,
+    },
+    env: {
+        schema: {
+            EMAIL_CONTACT: envField.string({
+                context: "client",
+                access: "public",
+            }),
+        },
+    },
 });

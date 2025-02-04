dec096324aadb5ba253786f72385588954dfb28c
anonymze <anodevfr@gmail.com>
2025-02-04 22:21:29 +0100

    ini

--- api/trigger-deploy.js
+++ api/trigger-deploy.js
@@ -1,4 +1,9 @@
-export async function GET() {
+export async function GET(req) {
+
+	if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+		return res.status(401).end('Unauthorized');
+	}
+
 	await fetch("https://api.vercel.com/v1/integrations/deploy/prj_5IzJAWAbUpd7AJpRAWqRLDPkP91T/gUWxx2NKgE");
 	return new Response("OK", { status: 200 });
 }

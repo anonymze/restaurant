5e220ae7c6707f90ad43ad4962c002ded321f22d
anonymze <anodevfr@gmail.com>
2025-02-05 13:09:02 +0100

    ini

--- api/trigger-deploy.js
+++ api/trigger-deploy.js
@@ -6,4 +6,5 @@ export async function GET(req) {
 
 	await fetch("https://api.vercel.com/v1/integrations/deploy/prj_5IzJAWAbUpd7AJpRAWqRLDPkP91T/gUWxx2NKgE");
 	return new Response("OK", { status: 200 });
+	
 }

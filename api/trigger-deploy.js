440c3c1f5535adef235d08b52a4951aa4ca4e300
anonymze <anodevfr@gmail.com>
2025-03-05 21:17:45 +0100

    add: signature library

--- api/trigger-deploy.js
+++ api/trigger-deploy.js
@@ -3,7 +3,7 @@ export async function GET(req) {
 		return new Response("KO", { status: 401 });
 	}
 
-	await fetch("https://api.vercel.com/v1/integrations/deploy/prj_5IzJAWAbUpd7AJpRAWqRLDPkP91T/gUWxx2NKgE");
+	await fetch("https://api.vercel.com/v1/integrations/deploy/prj_5IzJAWAbUpd7AJpRAWqRLDPkP91T/WeiZqkkowy");
 	return new Response("OK", { status: 200 });
 	
 }

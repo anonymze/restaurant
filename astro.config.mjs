01cfa7315fd6d6dff33257b5893455a50316a066
anonymze <anodevfr@gmail.com>
2025-03-18 08:13:33 +0100

    add: signature library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig, envField } from "astro/config";
+import simpleStackQuery from "simple-stack-query";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel";
@@ -7,42 +8,45 @@ import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
 
-import simpleStackQuery from "simple-stack-query";
-
-
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-    locales: ["fr", "en"],
-    defaultLocale: "fr",
-  },
-    site: "https://www.mauvaise-herbe-restaurant.fr",
-    adapter: vercel({
-        webAnalytics: {
-            enabled: true,
-        },
-    }),
-    output: "static",
-    integrations: [react(), sitemap(), sanity({
-        projectId: "ld33nh8g",
-        dataset: "production",
-        useCdn: false, // for static builds, better for development when content changes frequently
-		}), simpleStackQuery()],
-    vite: {
-        // @ts-ignore
-        plugins: [tailwindcss()],
-    },
+	i18n: {
+		locales: ["fr", "en"],
+		defaultLocale: "fr",
+	},
+	devToolbar: { enabled: false },
+	site: "https://www.mauvaise-herbe-restaurant.fr",
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
+	output: "static",
+	integrations: [
+		react(),
+		sitemap(),
+		sanity({
+			projectId: "ld33nh8g",
+			dataset: "production",
+			useCdn: false, // for static builds, better for development when content changes frequently
+		}),
+		simpleStackQuery(),
+	],
+	vite: {
+		// @ts-ignore
+		plugins: [tailwindcss()],
+	},
 
-    experimental: {
-        responsiveImages: true,
-        svg: true,
-    },
-    env: {
-        schema: {
-            EMAIL_CONTACT: envField.string({
-                context: "client",
-                access: "public",
-            }),
-        },
-    },
+	experimental: {
+		responsiveImages: true,
+		svg: true,
+	},
+	env: {
+		schema: {
+			EMAIL_CONTACT: envField.string({
+				context: "client",
+				access: "public",
+			}),
+		},
+	},
 });
